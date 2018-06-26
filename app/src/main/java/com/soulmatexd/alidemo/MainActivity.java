3cee33f2c85b42f7534d7fa744c2e19e36f8b008
SoulMateXD <975688206@qq.com>
2018-06-26 09:01:47 +0800

    添加了内存泄漏分析的demo和反编译时需要使用的代码

--- app/src/main/java/com/soulmatexd/alidemo/MainActivity.java
+++ app/src/main/java/com/soulmatexd/alidemo/MainActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
@@ -13,24 +14,46 @@ import com.alibaba.android.vlayout.VirtualLayoutManager;
 
 public class MainActivity extends ListActivity {
 
+    private String one="asasaas";
+
     private String[] mTitles = new String[]{
             VLayoutActivity.class.getSimpleName(),
             TangramActivity.class.getSimpleName(),
+            MemoryLeakActivity1.class.getSimpleName(),
     };
 
     private Class[] mActivities = new Class[]{
             VLayoutActivity.class,
             TangramActivity.class,
+            MemoryLeakActivity1.class,
     };
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setListAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, mTitles));
+        InnerClass innerClass = new InnerClass();
+        innerClass.printOuterClassPrivateFields();
     }
 
     @Override
     protected void onListItemClick(ListView l, View v, int position, long id) {
         startActivity(new Intent(this, mActivities[position]));
+    }
+
+    private void useOuterPrivateMethod(){
+        Log.d("MainActivity", "use outer private method");
+    }
+
+    public class InnerClass
+    {
+        public InnerClass() {}
+
+        public void printOuterClassPrivateFields()
+        {
+            String field = "one=" + MainActivity.this.one;
+            Log.d("MainActivity", "use outer attribute field = " + field);
+            MainActivity.this.useOuterPrivateMethod();
+        }
     }
 }
